ef07c102a0b1d28071f8ee8b232a8b16d89c5da3
media12 <56854971+media12@users.noreply.github.com>
2019-10-30 06:22:50 -0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-//config.js
-//ملف الكونفج
-module.exports = {
-    TOKEN: 'NjEwMzI0MDc3ODE4OTM3MzQ1.XblzlA.T2wW3ohdVBVbG0HniY4NcZ0IQCg',    
-    YT_API_KEY: 'api ',
-    prefix: '2',
-    devs: ['524098806296477697']
-}
